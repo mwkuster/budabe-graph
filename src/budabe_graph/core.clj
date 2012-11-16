79bc9d7256a9c16a7363843995d3c0200fa9a3e5
mwkuster <marc@budabe.eu>
2012-11-16 12:26:26 +0100

    Initial commit for session 7

--- src/budabe_graph/core.clj
+++ src/budabe_graph/core.clj
@@ -1,56 +1,32 @@
-(ns budabe-graph.core
-  (:use [compojure.core :only (GET PUT POST defroutes)])
-  (:require [compojure.route :as route]
-            [ring.util.response :as response]
-            [cheshire.core :as json]))
+(ns budabe-graph.core)
 
-(defrecord User [id, username, first_name, last_name,  gender, country])
+(defrecord User [username, first_name, last_name, gender, locale])
 
-(defrecord Relationship [id, type, from_user, to_user])
+(defrecord Connection [id, type, from_object, to_object])
 
 (def user-db (ref {}))
 
-(defn create-user [id username first_name last_name  gender country]
-  "Create a user and add her to the internal database"
+(defn create-user [username, first_name, last_name, gender, locale]
+  "Create a user and add to the internal database"
   (let
-      [user (->User id username first_name last_name  gender country)]
-    (dosync
-     (when-not (@user-db id)
-       (alter user-db assoc id user)))))
-
-(defn update-user [id username first_name last_name  gender country]
-  "Update the values for an existing user in the internal database"
-  (if (get @user-db id)
-    (let
-        [user (->User id username first_name last_name  gender country)]
-      (dosync
-       (alter user-db assoc id user)))))
-
-(defn get-user-by-id [id]
-  (get @user-db id))
-
-(defn list-users []
-  (vals @user-db))
+      [user (->User username, first_name, last_name, gender, locale)]
+    (dosync 
+     (when-not (@user-db username)
+       (alter user-db assoc username user)))))
 
 (defn get-user-by-username [username]
-  (filter #(= (:username %) username) (list-users)))
+  "Get back a user record by its username"
+  (get @user-db username))
 
-;(defn relate-users [])
-
-(defn list-users-handler []
-  {:status 200
-   :body (json/generate-string (list-users))})
-
-(defn get-user-by-id-handler [id]
-  {:status 200
-   :body (json/generate-string (get-user-by-id id))})
-
-(defroutes app
-  (GET "/list" request "Hello")
-  (GET ["/user/:id" :id #"\d+"] [id] (get-user-by-id-handler id)))
-
-
-
-
-
-
+(defn update-user [username first_name, last_name, gender, locale]
+  "Update the values for an existing user in the internal database"
+  (if (get @user-db username)
+    (let
+        [user (->User username, first_name, last_name, gender, locale)]
+      (dosync
+       (alter user-db assoc username user)))))
+       
+(defn list-users []
+  "Return the information for all existing users in the internal database"
+  (vals @user-db))
+   
--- test/budabe_graph/core_test.clj
+++ test/budabe_graph/core_test.clj
@@ -2,43 +2,22 @@
   (:use clojure.test
         budabe-graph.core))
 
-;; (defn reset-db []
-;;   (dosync
-;;     (alter user-db {})))
-
-;; (defn reset-fixture [f]
-;;   (reset-db)
-;;   (f))
-
-;;(use-fixtures :each reset-fixture)
-
 (deftest user-test
-  (testing "Test the create user functionality"
-    (is 
-     (= (count @user-db) 0))
-    (create-user 1 "johnsmith" "John" "Smith" "male" "US")
-    (is 
-     (= (count @user-db) 1))
-    (is 
-     (= (:username (get @user-db 1)) "johnsmith"))
-    ;you cannot create the same user twice
-    (create-user 1 "johnsmith" "John" "Smith" "male" "US")
-    (is 
-     (= (count @user-db) 1))
-    (create-user 2 "janegreen" "Jane" "Green" "female" "US")
-    (is 
-     (= (count @user-db) 2)))
-  (testing "Test the list user functionality"
-    (is
-     (= (sort (keys @user-db)) '(1 2)))
-    (is 
-     (= (set (list-users)) (set (list (->User 1 "johnsmith" "John" "Smith" "male" "US") (->User 2 "janegreen" "Jane" "Green" "female" "US"))))))
-  (testing "Test the update user functionality"
-    (update-user 1 "johnsmith" "James" "Smith" "male" "FR")
-    (is
-     (= (:first_name (get @user-db 1)) "James"))
-    (is 
-     (= (count @user-db) 2))))
-    
-    
-    
+  (testing "Test the create-user and get-user-by-username functions"
+    (is (= (count @user-db) 0))
+    (create-user "johnsmith" "John" "Smith" "male" "US")
+    (is (= (count @user-db) 1))
+    (create-user "johnsmith" "John" "Smith" "male" "US")
+    (is (= (count @user-db) 1))
+    (create-user "janegreen" "Jane" "Green" "female" "DE")
+    (is (= (count @user-db) 2))    
+    (is (= (:last_name (get-user-by-username "janegreen") "Green"))))
+
+  (testing "Test the update-user function"
+    (update-user "johnsmith" "John" "Brown" "male" "FR")
+    (is (= (:last_name (get-user-by-username "johnsmith") "Brown"))))
+
+  (testing "Test the list-users function"
+    (is (= (set (map :last_name (list-users))) (set '("Green" "Brown")))))
+
+)
